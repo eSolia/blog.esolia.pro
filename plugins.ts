6d9d3eadc6b63ecba685f8d398a6623280e21534
James R. Cogley <rick.cogley@esolia.co.jp>
2025-02-17 15:29:58 +0900

    try implementing with plugins.ts in situ

--- plugins.ts
+++ plugins.ts
@@ -1,8 +1,6 @@
-import date, { Options as DateOptions } from "lume/plugins/date.ts";
-import postcss from "lume/plugins/postcss.ts";
-import terser from "lume/plugins/terser.ts";
-import prism, { Options as PrismOptions } from "lume/plugins/prism.ts";
-import basePath from "lume/plugins/base_path.ts";
+
+
+
 import slugifyUrls from "lume/plugins/slugify_urls.ts";
 import resolveUrls from "lume/plugins/resolve_urls.ts";
 import metas from "lume/plugins/metas.ts";
@@ -11,10 +9,12 @@ import sitemap from "lume/plugins/sitemap.ts";
 import feed, { Options as FeedOptions } from "lume/plugins/feed.ts";
 import readingInfo from "lume/plugins/reading_info.ts";
 import { merge } from "lume/core/utils/object.ts";
+import title from "https://deno.land/x/lume_markdown_plugins@v0.7.1/title.ts";
 import toc from "https://deno.land/x/lume_markdown_plugins@v0.8.0/toc.ts";
 import image from "https://deno.land/x/lume_markdown_plugins@v0.8.0/image.ts";
 import footnotes from "https://deno.land/x/lume_markdown_plugins@v0.8.0/footnotes.ts";
 import { alert } from "npm:@mdit/plugin-alert@0.14.0";
+import multilanguage from "lume/plugins/multilanguage.ts";
 
 import "lume/types.ts";
 
@@ -23,6 +23,8 @@ export interface Options {
   date?: Partial<DateOptions>;
   pagefind?: Partial<PagefindOptions>;
   feed?: Partial<FeedOptions>;
+  languages?: string[];
+  languageNames?: Record<string, string>;
 }
 
 export const defaults: Options = {
@@ -44,8 +46,9 @@ export default function (userOptions?: Options) {
   const options = merge(defaults, userOptions);
 
   return (site: Lume.Site) => {
-    site.use(postcss())
+    site.use(lightningCss())
       .use(basePath())
+      .use(title())
       .use(toc())
       .use(prism(options.prism))
       .use(readingInfo())
