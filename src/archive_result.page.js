ba68a93379562bf9f389ce734bd73b9439c96630
James R. Cogley <rick.cogley@esolia.co.jp>
2025-02-17 21:13:11 +0900

    specify lang

--- src/archive_result.page.js
+++ src/archive_result.page.js
@@ -3,23 +3,23 @@ export const lang = "ja";
 
 export default function* ({ search, i18n }) {
   // Generate a page for each tag
-  for (const tag of search.values("tags")) {
+  for (const tag of search.values("tags", `lang=${lang}`)) {
     yield {
       url: `/archive/${tag}/`,
       title: `${i18n.search.by_tag}  “${tag}”`,
       type: "tag",
-      search_query: `type=post '${tag}'`,
+      search_query: `type=post lang=${lang} '${tag}'`,
       tag,
     };
   }
 
   // Generate a page for each author
-  for (const author of search.values("author")) {
+  for (const author of search.values("author", `lang=${lang}`)) {
     yield {
       url: `/author/${author}/`,
       title: `${i18n.search.by_author} ${author}`,
       type: "author",
-      search_query: `type=post author='${author}'`,
+      search_query: `type=post lang=${lang} author='${author}'`,
       author,
     };
   }
