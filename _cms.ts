3924f74025651930b5c0c1a7216a6eb36f5c5605
James R. Cogley <rick.cogley@esolia.co.jp>
2025-02-27 10:08:12 +0900

    Hide extraneous sections

--- _cms.ts
+++ _cms.ts
@@ -56,78 +56,78 @@ const url: Field = {
   },
 };
 
-cms.document(
-  "settings: Global settings for the site",
-  "src:_data.yml",
-  [
-    {
-      name: "lang",
-      type: "select",
-      label: "Language",
-      description: "コンテンツの言語を選択する<br>Select the language of the page content",
-      attributes: {
-        required: true,
-      },
-      options: [
-        {
-          label: "日本語",
-          value: "ja"
-        },
-        {
-          label: "English",
-          value: "en"
-        },
-      ],
-    },
-    {
-      name: "home",
-      type: "object",
-      fields: [
-        {
-          name: "welcome",
-          type: "text",
-          label: "Title",
-          description: "Welcome message in the homepage",
-        },
-      ],
-    },
-    {
-      name: "menu_links",
-      type: "object-list",
-      fields: [
-        {
-          name: "title",
-          type: "text",
-          label: "Title",
-        },
-        {
-          name: "url",
-          type: "text",
-          label: "URL",
-        },
-      ],
-    },
-    {
-      name: "extra_head",
-      type: "code",
-      description: "Extra content to include in the <head> tag",
-    },
-    {
-      name: "metas",
-      type: "object",
-      description: "Meta tags configuration.",
-      fields: [
-        "site: text",
-        "description: text",
-        "title: text",
-        "image: text",
-        "twitter: text",
-        "lang: text",
-        "generator: checkbox",
-      ],
-    },
-  ],
-);
+// cms.document(
+//   "settings: Global settings for the site",
+//   "src:_data.yml",
+//   [
+//     {
+//       name: "lang",
+//       type: "select",
+//       label: "Language",
+//       description: "コンテンツの言語を選択する<br>Select the language of the page content",
+//       attributes: {
+//         required: true,
+//       },
+//       options: [
+//         {
+//           label: "日本語",
+//           value: "ja"
+//         },
+//         {
+//           label: "English",
+//           value: "en"
+//         },
+//       ],
+//     },
+//     {
+//       name: "home",
+//       type: "object",
+//       fields: [
+//         {
+//           name: "welcome",
+//           type: "text",
+//           label: "Title",
+//           description: "Welcome message in the homepage",
+//         },
+//       ],
+//     },
+//     {
+//       name: "menu_links",
+//       type: "object-list",
+//       fields: [
+//         {
+//           name: "title",
+//           type: "text",
+//           label: "Title",
+//         },
+//         {
+//           name: "url",
+//           type: "text",
+//           label: "URL",
+//         },
+//       ],
+//     },
+//     {
+//       name: "extra_head",
+//       type: "code",
+//       description: "Extra content to include in the <head> tag",
+//     },
+//     {
+//       name: "metas",
+//       type: "object",
+//       description: "Meta tags configuration.",
+//       fields: [
+//         "site: text",
+//         "description: text",
+//         "title: text",
+//         "image: text",
+//         "twitter: text",
+//         "lang: text",
+//         "generator: checkbox",
+//       ],
+//     },
+//   ],
+// );
 
 cms.collection({
   name: "posts",
@@ -286,49 +286,49 @@ REPLACE ME. Enter your content here, using **markdown** formatting of _any kind_
   ],
 });
 
-cms.collection(
-  "pages: Additional pages, like about, contact, etc.",
-  "src:pages/*.md",
-  [
-    {
-      name: "layout",
-      type: "hidden",
-      value: "layouts/page.vto",
-    },
-    {
-      name: "title",
-      type: "text",
-      label: "Title",
-    },
-    url,
-    {
-      name: "menu",
-      type: "object",
-      label: "Whether to include in the menu",
-      fields: [
-        {
-          name: "visible",
-          type: "checkbox",
-          label: "Show in menu",
-        },
-        {
-          name: "order",
-          type: "number",
-          label: "Order",
-        },
-      ],
-    },
-    {
-      name: "extra_head",
-      type: "code",
-      description: "Extra content to include in the <head> tag",
-    },
-    {
-      name: "content",
-      type: "markdown",
-      label: "Content",
-    },
-  ],
-);
+// cms.collection(
+//   "pages: Additional pages, like about, contact, etc.",
+//   "src:pages/*.md",
+//   [
+//     {
+//       name: "layout",
+//       type: "hidden",
+//       value: "layouts/page.vto",
+//     },
+//     {
+//       name: "title",
+//       type: "text",
+//       label: "Title",
+//     },
+//     url,
+//     {
+//       name: "menu",
+//       type: "object",
+//       label: "Whether to include in the menu",
+//       fields: [
+//         {
+//           name: "visible",
+//           type: "checkbox",
+//           label: "Show in menu",
+//         },
+//         {
+//           name: "order",
+//           type: "number",
+//           label: "Order",
+//         },
+//       ],
+//     },
+//     {
+//       name: "extra_head",
+//       type: "code",
+//       description: "Extra content to include in the <head> tag",
+//     },
+//     {
+//       name: "content",
+//       type: "markdown",
+//       label: "Content",
+//     },
+//   ],
+// );
 
 export default cms;
