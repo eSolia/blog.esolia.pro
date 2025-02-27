9803c1a2fe1fe7f46b99c469465701f18525c49c
James R. Cogley <rick.cogley@esolia.co.jp>
2025-02-27 09:39:18 +0900

    revert env

--- _cms.ts
+++ _cms.ts
@@ -1,8 +1,8 @@
 import lumeCMS from "lume/cms/mod.ts";
 import { Field } from "lume/cms/types.ts";
 
-const username = Deno.env.get("ESBLOG_U1")!;
-const password = Deno.env.get("ESBLOG_P1")!;
+// const username = Deno.env.get("ESBLOG_U1")!;
+// const password = Deno.env.get("ESBLOG_P1")!;
 
 const cms = lumeCMS({
   site: {
