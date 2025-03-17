dc2e6e6bb7c2e635dfccc3b6126e069ecb41d696
James R. Cogley <rick.cogley@esolia.co.jp>
2025-03-17 19:19:48 +0900

    Add error check

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,21 @@
-window.addEventListener('load', (event) => {
+// window.addEventListener('load', (event) => {
+//   document.querySelectorAll('.data-fatrigger').forEach(item => {
+//     item.addEventListener('click', event => {
+//       let postId = item.getAttribute('data-faid');
+//       fathom.trackEvent(`post link click: ${postId}`);
+//     });
+//   });
+// });
+
+window.addEventListener('load', () => {
   document.querySelectorAll('.data-fatrigger').forEach(item => {
     item.addEventListener('click', event => {
       let postId = item.getAttribute('data-faid');
-      fathom.trackEvent(`post link click: ${postId}`);
+      if (typeof fathom !== 'undefined') {
+        fathom.trackEvent(`post link click: ${postId}`);
+      } else {
+        console.error('Fathom Analytics script not loaded');
+      }
     });
   });
 });
