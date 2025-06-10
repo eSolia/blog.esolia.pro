52a9ee78a09121f3254bf9e6924da34d2ef8aead
James R. Cogley <rick.cogley@esolia.co.jp>
2025-06-11 05:43:30 +0900

    Loads separate google fonts per lang
    Specifies plugin twice
    Loads different fonts for each lang
    Tells plugin which subsets to ignore, instead of which to include (better for ja because of volume of them)
    Refactors sed command to handle repathing for both langs

--- _config.ts
+++ _config.ts
@@ -127,133 +127,25 @@ site.use(prism({
 // CSS + JS + source maps
 site.use(esbuild());
 site.use(googleFonts({
-  subsets: [
-    "latin",
-    "latin-ext",
-    "[2]",
-    "[3]",
-    "[4]",
-    "[5]",
-    "[6]",
-    "[7]",
-    "[8]",
-    "[9]",
-    "[10]",
-    "[11]",
-    "[12]",
-    "[13]",
-    "[14]",
-    "[15]",
-    "[16]",
-    "[17]",
-    "[18]",
-    "[19]",
-    "[20]",
-    "[21]",
-    "[22]",
-    "[23]",
-    "[24]",
-    "[25]",
-    "[26]",
-    "[27]",
-    "[28]",
-    "[29]",
-    "[30]",
-    "[31]",
-    "[32]",
-    "[33]",
-    "[34]",
-    "[35]",
-    "[36]",
-    "[37]",
-    "[38]",
-    "[39]",
-    "[40]",
-    "[41]",
-    "[42]",
-    "[43]",
-    "[44]",
-    "[45]",
-    "[46]",
-    "[47]",
-    "[48]",
-    "[49]",
-    "[50]",
-    "[51]",
-    "[52]",
-    "[53]",
-    "[54]",
-    "[55]",
-    "[56]",
-    "[57]",
-    "[58]",
-    "[59]",
-    "[60]",
-    "[61]",
-    "[62]",
-    "[63]",
-    "[64]",
-    "[65]",
-    "[66]",
-    "[67]",
-    "[68]",
-    "[69]",
-    "[70]",
-    "[71]",
-    "[72]",
-    "[73]",
-    "[74]",
-    "[75]",
-    "[76]",
-    "[77]",
-    "[78]",
-    "[79]",
-    "[80]",
-    "[81]",
-    "[82]",
-    "[83]",
-    "[84]",
-    "[85]",
-    "[86]",
-    "[87]",
-    "[88]",
-    "[89]",
-    "[90]",
-    "[91]",
-    "[92]",
-    "[93]",
-    "[94]",
-    "[95]",
-    "[96]",
-    "[97]",
-    "[98]",
-    "[99]",
-    "[100]",
-    "[101]",
-    "[102]",
-    "[103]",
-    "[104]",
-    "[105]",
-    "[106]",
-    "[107]",
-    "[108]",
-    "[109]",
-    "[110]",
-    "[111]",
-    "[112]",
-    "[113]",
-    "[114]",
-    "[115]",
-    "[116]",
-    "[117]",
-    "[118]",
-    "[119]",
-  ],
-  cssFile: "styles.css",
-  placeholder: "/* lume-google-fonts-here */",
+  cssFile: "fonts-ja.css",
+  fontsFolder: "fonts-ja",
+  ignoredSubsets: ["cyrillic", "cyrillic-ext", "vietnamese"],
   fonts: {
     textface:
       "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+JP:wght@100;200;300;400;500;600;700&display=swap",
+    codeface:
+      "https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,400;0,600;1,400;1,600&display=swap",
+  },
+}));
+site.use(googleFonts({
+  cssFile: "fonts-en.css",
+  fontsFolder: "fonts-en",
+  ignoredSubsets: ["cyrillic", "cyrillic-ext", "vietnamese"],
+  fonts: {
+    textface:
+      "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap",
+    codeface:
+      "https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,400;0,600;1,400;1,600&display=swap",
   },
 }));
 site.use(tailwindcss({
@@ -478,6 +370,22 @@ site.process([".html"], deferPagefind());
 // pass the base url 
 site.process([".html"], externalLinksIcon("https://blog.esolia.pro"));
 
+// Define bash script to fix English font URLs
+const sedFixFontpathEn = Deno.build.os === "darwin"
+  ? "sed -i '' 's|url(\"fonts-en/|url(\"/fonts-en/|g' _site/fonts-en.css"
+  : "sed -i 's|url(\"fonts-en/|url(\"/fonts-en/|g' _site/fonts-en.css";
+
+// Define bash script to fix Japanese font URLs
+const sedFixFontpathJa = Deno.build.os === "darwin"
+  ? "sed -i '' 's|url(\"fonts-ja/|url(\"/fonts-ja/|g' _site/fonts-ja.css"
+  : "sed -i 's|url(\"fonts-ja/|url(\"/fonts-ja/|g' _site/fonts-ja.css";
+
+// Register commands
+site.script("makeFontpathAbsoluteEn", sedFixFontpathEn);
+site.script("makeFontpathAbsoluteJa", sedFixFontpathJa);
+// Execute scripts after build
+site.addEventListener("afterBuild", "makeFontpathAbsoluteEn");
+site.addEventListener("afterBuild", "makeFontpathAbsoluteJa");
 
 // site.filter("tdate", (value: string | undefined, locale: string, timezone: string) => {
 //   if (!value) {
